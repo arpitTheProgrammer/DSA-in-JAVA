48d9ccb098591a545bb5dd98d9ad07288a2b7b77
arpitTheProgrammer <arpitsrivastava8863.9h@gmail.com>
2025-01-13 01:04:05 +0530

    Basics of Linked List

---
+++ LLpractice2.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class LLpractice2 {
+    static class Node {
+        String data;;
+        Node next;
+        Node(String data){
+         this.data = data;
+         this.next = null;   
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        // list.addFirst("Hayy");
+        // list.addFirst("Hello");
+        // System.out.print(list.size() + " ->  ");
+
+        // for(int i = 0; i < list.size(); i++){
+        //     System.out.print(list.get(i) + " -> ");
+        // }
+        // System.out.print("null");
+
+        // // list.removeFirst();
+        // // System.out.print(list);
+
+        // System.out.println(list.getFirst());
+
+        list.add("BMW");
+        list.add("AUDI");
+        list.add("TATA");
+        list.add("SUZUKI");
+
+        Collections.sort(list);
+        for(String i : list){
+            System.out.println(i);
+        }
+
+            ArrayList<Integer> num = new ArrayList<Integer>();
+            num.add(23);
+            num.add(13);
+            num.add(53);
+            num.add(33);
+            num.add(26);
+
+            Collections.sort(num); // It will sort in arranging order
+            Collections.sort(num, Collections.reverseOrder()); // Use to Arrange in Descending Order
+            for(int i : num){
+                System.out.println(i);
+            }
+
+    }
+}
